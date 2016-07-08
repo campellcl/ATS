6417fc1b3e8bf2065c7972c9e44ca5b0f39d6454
Chris Campell <ccampell.asu@gmail.com>
2016-07-08 08:17:51 -0400

    Added Scrapy Web Scraper
    Created a class attempting to use Scrapy as a web scraper. Permission
    errors and workspace build errors are too pervasive to continue.

---
+++ Program/ScrapyWebScraper.py
@@ -0,0 +1,36 @@
+"""
+ScrapyWebScraper.py
+Scrapes the Appalachian trail journal website for relevant information using the Scrapy package.
+ @Author: Chris Campell
+ @Version: 7/5/2016
+"""
+
+import os
+import sys
+import Scrapy
+
+class ScrapyWebScraper(object):
+
+    def __init__(self):
+        start_url = "http://www.trailjournals.com/entry.cfm?trailname="
+        at_hikers = open("at-hikers.txt", 'r')
+        storage_location = "C:/Users/Chris/Documents/GitHub/ATS/Data/Hiker_Data"
+        for line in iter(at_hikers):
+            hiker_fname = storage_location + "/" + str.strip(line, '\n') + ".json"
+            if not os.path.isfile(hiker_fname):
+                hiker_url = start_url + line
+                hiker_info = Selector(base_url=hiker_url)
+                hiker = recordHikerInfo(hiker_id=int(line), journal_url=hiker_url)
+                hiker = parseHikerJournal(hiker, journal_url=hiker_url)
+                writeHiker(hiker)
+            else:
+                print("Hiker id: %d has already been logged." % int(line))
+        at_hikers.close()
+
+def main(cmd_args):
+    pass
+
+if __name__ == '__main__':
+    main(sys.argv)
+
+
