34026bf687cd577625fad03d2230159ed8eb168a
Chris Campell <ccampell.asu@gmail.com>
2016-07-08 08:20:53 -0400

    Updates
    File updates during debugging.

--- Program/HikerRecorder.py
+++ Program/HikerRecorder.py
@@ -37,20 +37,38 @@ isValidStorageLocation -Determines if the provided file path is recognizable by 
 @return boolean -True if valid file path, false otherwise.
 """
 def isValidStorageLocation(storage_location):
-     if not os.path.isdir(storage_location):
+    if not os.path.isdir(storage_location):
         print("HIKER RECORDER: Hiker Storage Location Not Recognized by OS!")
         return False
-     else:
-         return True
+    else:
+        return True
 
 """
 convertToCSV -Converts the hiker journal to a csv friendly format, removing pre-existing commas.
 @param hiker_journal -The journal to be converted to a csv friendly format.
-@return csv_hiker_journal -An identical hiker journal now in csv format (commas removed). 
+@return csv_hiker_journal -An identical hiker journal now in csv format (commas removed).
 """
 def convertToCSV(hiker_journal):
     csv_hiker_journal = copy.deepcopy(hiker_journal)
-    csv_hiker_journal['date'] = csv_hiker_journal['date'].replace(",", " ")
+    for key, value in csv_hiker_journal.items():
+        if value['date'] is not None:
+            value['date'] = value['date'].replace("\"", "\'")
+            value['date'] = ("\"" + value['date'] + "\"")
+        else:
+            value['date'] = "None"
+        if value['start_loc'] is not None:
+            value['start_loc'] = value['start_loc'].replace("\"", "\'")
+            value['start_loc'] = ("\"" + value['start_loc'] + "\"")
+        else:
+            value['start_loc'] = "None"
+        if value['dest'] is not None:
+            value['dest'] = value['dest'].replace("\"", "\'")
+            value['dest'] = ("\"" + value['dest'] + "\"")
+        else:
+            value['dest'] = "None"
+        # value['date'] = value['date'].replace(",", "")
+        # value['start_loc'] = value['start_loc'].replace(",", "")
+        # value['dest'] = value['dest'].replace(",", "")
     return csv_hiker_journal
 
 def main(cmd_args):
@@ -62,22 +80,25 @@ def main(cmd_args):
     hiker_csv = open('hikers.csv', 'w')
     # Write CSV header:
     hiker_csv.write("id,entry,date,start,dest,day_mileage,trip_mileage\n")
-
     for line in iter(at_hikers):
         hiker_fname = storage_location + "/" + str.strip(line, '\n') + ".json"
-        if not os.path.isfile(hiker_fname):
+        if os.path.isfile(hiker_fname):
+            with open(hiker_fname, 'r') as fp:
+                hiker_data = json.load(fp=fp)
+                print("Writing Hiker: %s to CSV file." % hiker_data['identifier'])
+                # hiker_csv.write(str(hiker_data['identifier']) + ",")
+                hiker_id = hiker_data['identifier']
+                hiker_journal = hiker_data['journal']
+                if hiker_journal:
+                    hiker_journal = sortHikerJournal(hiker_journal=hiker_journal)
+                    hiker_journal = convertToCSV(hiker_journal)
+                    for key, value in hiker_journal.items():
+                        hiker_csv.write(str(hiker_id) + "," + str(key) + "," + value['date'] + "," + value['start_loc'] + "," + value['dest'] + "," + str(value['day_mileage']) + "," + str(value['trip_mileage']) + "\n")
+                else:
+                    pass
+        else:
             print("HIKER RECORDER: Hiker File: %s not found." % hiker_fname)
-
-        with open(hiker_fname, 'r') as fp:
-            hiker_data = json.load(fp=fp)
-            # hiker_csv.write(str(hiker_data['identifier']) + ",")
-            hiker_journal = hiker_data['journal']
-            hiker_journal = sortHikerJournal(hiker_journal=hiker_journal)
-            hiker_journal = convertToCSV(hiker_journal)
-            hiker_id = hiker_data['identifier']
-            for key, value in hiker_journal.items():
-                hiker_csv.write(str(hiker_id) + "," + str(key) + "," + value['date'] + "," + value['start_loc'] + "," + value['dest'] + "," + str(value['day_mileage']) + "," + str(value['trip_mileage']) + "\n")
-            hiker_csv.close()
+    hiker_csv.close()
     at_hikers.close()
 
 
